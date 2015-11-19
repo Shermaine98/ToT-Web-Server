e386ac915389b4e317a5c6da4cb57b87589e4552
shermainesy <shermaine_sy@dlsu.edu.ph>
2015-11-19 14:46:41 +0800

    db

---
+++ ToT/src/java/Database/DBConnectionFactory.java
@@ -0,0 +1,32 @@
+package Database;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Atayan
+ * @author Lapidario
+ * @author Sy
+ * @author Nunez
+ *
+ */
+public abstract class DBConnectionFactory {
+
+    String url = "jdbc:mysql://127.0.0.1:3306/MOBIDEV";
+    String username = "MOBIDEV";
+    String password = "ToT";
+
+    /**
+     *
+     * @return
+     */
+    public static DBConnectionFactory getInstance() {
+        return new DBConnectionFactoryImpl();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public abstract Connection getConnection();
+}
---
+++ ToT/src/java/Database/DBConnectionFactoryImpl.java
@@ -0,0 +1,37 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Atayan
+ * @author Lapidario
+ * @author Sy
+ * @author Nunez
+ *
+ */
+public class DBConnectionFactoryImpl extends DBConnectionFactory {
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            return conn;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBConnectionFactoryImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnectionFactoryImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+}
